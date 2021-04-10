7d6203b3554313a27cf69b9004c954050552eedc
pateba06 <34305933+pateba06@users.noreply.github.com>
2021-04-10 00:45:32 -0400

    routing

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,42 +1,28 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 import { LearnngifelsethenComponent } from './learning/learnngifelsethen/learnngifelsethen.component';
 import { NgforlearningComponent } from './ngforlearning/ngforlearning.component';
 import { PushslicengforComponent } from './pushslicengfor/pushslicengfor.component';
 import { RoutelearningComponent } from './routelearning/routelearning.component';
-
 import { RouterModule, Routes } from '@angular/router';
-
 //will make constant appRoutes and import Routes in module.ts
 import { NotfoundComponent } from './notfound/notfound.component';
 import { LoginpageComponent } from './loginpage/loginpage.component';
-import { AppRoutingModule } from './app-routing.module';
 
 
-
+const appRoutes:Routes=[
+  { path:'', redirectTo:'login', pathMatch:'full'},  
+  { path:'home', component:RoutelearningComponent},
+  { path:'login', component:LoginpageComponent},
+  { path:'learning', component:LearnngifelsethenComponent},
+  { path:'ngfor', component:NgforlearningComponent},
+  { path:'pushandslice', component:PushslicengforComponent},
+  { path:'**', component:NotfoundComponent} // wild card route
+]
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      LearnngifelsethenComponent,
-      NgforlearningComponent,
-      PushslicengforComponent,
-      RoutelearningComponent,
-      NotfoundComponent,
-      LoginpageComponent
-   ],
-   imports: [
-
-	 BrowserModule,
-	 FormsModule,
-	 AppRoutingModule
-	],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [],
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
 })
-export class AppModule { }
+export class AppRoutingModule { }
